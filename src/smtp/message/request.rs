b6e03f552ccacfb7c097b64de05ea281124b46fd
tanndlin <tanndlin@gmail.com>
2026-08-20 18:42:19 -0400

    Fix swapped hello

--- src/smtp/message/request.rs
+++ src/smtp/message/request.rs
@@ -22,8 +22,8 @@ impl TryFrom<String> for Request {
     fn try_from(value: String) -> Result<Self, Self::Error> {
         let (command, rest) = value.split_once(' ').unwrap_or((value.as_str().trim(), ""));
         Ok(match command {
-            "EHLO" => Request::Hello(HelloMessage::from(rest)),
-            "HELO" => Request::EHello(ExtendedHelloMessage::from(rest)),
+            "HELO" => Request::Hello(HelloMessage::from(rest)),
+            "EHLO" => Request::EHello(ExtendedHelloMessage::from(rest)),
             "MAIL" => Request::Mail(MailMessage::from(rest)),
             "RCPT" => Request::Recipient(RecipientMessage::from(rest)),
             "DATA" => Request::Data(()),
