fc1be1192855c6348761d9046c498411565f5de3
tanndlin <tanndlin@gmail.com>
2026-08-20 18:41:06 -0400

    Stop double encoding CRLF

--- src/smtp/message/response.rs
+++ src/smtp/message/response.rs
@@ -11,9 +11,9 @@ impl EncodeTo for Response {
     fn encode_to(self, buf: &mut Vec<u8>) {
         match self {
             Response::Ready(ready) => ready.encode_to(buf),
-            Response::Ok(()) => buf.extend(b"250 OK\r\n"),
-            Response::Closing(()) => buf.extend(b"221 rs-smtp v0.1 closing channel\r\n"),
-            Response::StartMailInput(()) => buf.extend(b"354 start mail input\r\n"),
+            Response::Ok(()) => buf.extend(b"250 OK"),
+            Response::Closing(()) => buf.extend(b"221 rs-smtp v0.1 closing channel"),
+            Response::StartMailInput(()) => buf.extend(b"354 start mail input"),
         }
 
         buf.extend(b"\r\n");
