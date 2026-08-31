cd70fc89f4019eda09c2de0a7af9d0dc07873e4f
tanndlin <tanndlin@gmail.com>
2026-08-31 19:18:04 -0400

    Status command tests

--- imap/src/command/status_command.rs
+++ imap/src/command/status_command.rs
@@ -21,9 +21,11 @@ pub struct StatusCommand {
 
 impl ClientCommandTrait for StatusCommand {
     fn parse_bytes(tag: String, cursor: &mut Cursor) -> Result<Self, CommandParseError> {
-        let mailbox = cursor.atom().unwrap().to_string();
+        let mailbox = cursor.atom()?.to_string();
 
-        let flags = cursor.paren_list(|c| c.atom()).unwrap();
+        let flags = cursor.paren_list(|c| c.atom())?;
+        dbg!(&flags);
+
         let messages = flags.contains(&"MESSAGES");
         let next_uid = flags.contains(&"UIDNEXT");
         let validity_uid = flags.contains(&"UIDVALIDITY");
@@ -31,8 +33,8 @@ impl ClientCommandTrait for StatusCommand {
         let deleted = flags.contains(&"DELETED");
         let size = flags.contains(&"SIZE");
 
-        cursor.eat(b'\r');
-        cursor.eat(b'\n');
+        cursor.eat(b'\r')?;
+        cursor.eat(b'\n')?;
         Ok(Self {
             tag,
             mailbox,
@@ -47,3 +49,50 @@ impl ClientCommandTrait for StatusCommand {
 }
 
 client_command_from_impl!(StatusCommand, Status);
+
+#[cfg(test)]
+mod tests {
+    use crate::errors::ParseError;
+
+    use super::*;
+
+    #[test]
+    fn no_optionals_throws() {
+        let Err(e) = ClientCommand::parse_bytes(b"A042 STATUS blurdybloop\r\n") else {
+            panic!()
+        };
+        assert!(matches!(
+            e,
+            CommandParseError::ParseError(ParseError::UnexpectedChar(23))
+        ));
+    }
+
+    #[test]
+    fn parses_single_fetch_parameter() {
+        let (ClientCommand::Status(cmd), _) =
+            ClientCommand::parse_bytes(b"A042 STATUS blurdybloop MESSAGES\r\n").unwrap()
+        else {
+            panic!()
+        };
+
+        assert_eq!(cmd.tag, "A042");
+        assert_eq!(cmd.mailbox, "blurdybloop");
+        assert!(cmd.messages);
+        assert!(!cmd.next_uid);
+    }
+
+    #[test]
+    fn parses_all_optionals() {
+        let (ClientCommand::Status(cmd), _) = ClientCommand::parse_bytes(
+            b"A042 STATUS blurdybloop (MESSAGES UIDNEXT UIDVALIDITY UNSEEN DELETED SIZE)\r\n",
+        )
+        .unwrap() else {
+            panic!()
+        };
+
+        assert_eq!(cmd.tag, "A042");
+        assert_eq!(cmd.mailbox, "blurdybloop");
+        assert!(cmd.next_uid);
+        assert!(cmd.messages);
+    }
+}
