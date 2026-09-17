f9ac824c620532c4e7e93f5e609bcf187c6a34f2
tanndlin <tanndlin@gmail.com>
2026-09-16 22:08:44 -0400

    [IMAP-Create] Eat CRLF

--- imap/src/command/create_command.rs
+++ imap/src/command/create_command.rs
@@ -14,6 +14,10 @@ pub struct CreateCommand {
 impl ClientCommandTrait for CreateCommand {
     fn parse_bytes(tag: String, cursor: &mut Cursor) -> Result<Self, CommandParseError> {
         let mailbox = cursor.string()?.to_string();
+
+        cursor.eat(b'\r')?;
+        cursor.eat(b'\n')?;
+
         Ok(CreateCommand { tag, mailbox })
     }
 
